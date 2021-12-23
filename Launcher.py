5a10e7d8338b751e8a8dd94ec4f3db23a78cb33d
artem <artem00561@gmail.com>
2021-12-24 00:02:18 +0300

    += игнорирование масштабирования Windows

--- Launcher.py
+++ Launcher.py
@@ -2,9 +2,13 @@ import pygame
 from CONSTANTS import *
 from GameManager import GameManager
 from CONSTANTS import FPS, WIDTH, HEIGHT
+import ctypes
+
 
 def launch():
     '''Запускает игру'''
+
+    ctypes.windll.user32.SetProcessDPIAware() # игнорирование масштабирования Windows
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
     pygame.display.set_caption('Voice neutralization')
