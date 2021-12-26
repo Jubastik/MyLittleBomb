0f425c4b1b1aaef88acef976f0c6fb7416a79c69
Ru1ka <wotb5ks@gmail.com>
2021-12-26 11:12:48 +0300

    добавлено игноирование маштабирования виндовс

--- Launcher.py
+++ Launcher.py
@@ -2,16 +2,20 @@ import pygame
 from CONSTANTS import *
 from GameManager import GameManager
 from CONSTANTS import FPS, WIDTH, HEIGHT
+import ctypes
+
 
 def launch():
-    '''Запускает игру'''
+    """Запускает игру"""
+
+    ctypes.windll.user32.SetProcessDPIAware()  # игнорирование масштабирования Windows
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
-    pygame.display.set_caption('Voice neutralization')
+    pygame.display.set_caption("Voice neutralization")
     gm = GameManager(screen, FPS)
     gm.loop()
     pygame.quit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     launch()
