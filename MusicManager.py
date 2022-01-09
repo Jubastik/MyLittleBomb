38f8d826a8f2b584e628ac8f5c8624da67335fab
artem <artem00561@gmail.com>
2022-01-09 12:51:32 +0300

    добавлена музыка в end stage

--- MusicManager.py
+++ MusicManager.py
@@ -7,16 +7,21 @@ class MusicManager:
     def __init__(self):
         self.music_list = {
             "menu": "Resources/Music/MenuMusic.mp3",
+            "result": "Resources/Music/MenuMusic.mp3",
             "game": "Resources/Music/GameStart.mp3",
         }
         self.volume = 1.0
 
     def start_music(self, music):
         if music in self.music_list:
-            pygame.mixer.music.load(self.music_list[music])
-            if music == "menu":
+            if music == "menu" and not pygame.mixer.music.get_busy():
+                pygame.mixer.music.load(self.music_list[music])
                 pygame.mixer.music.play(-1)
-            else:
+            elif music == "result":
+                pygame.mixer.music.load(self.music_list[music])
+                pygame.mixer.music.play(-1)
+            elif music == "game":
+                pygame.mixer.music.load(self.music_list[music])
                 pygame.mixer.music.play()
 
     def change_volume(self, vol):
