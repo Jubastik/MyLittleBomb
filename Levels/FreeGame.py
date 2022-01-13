084e8415353d8259ac9d970e220a7157d9ada036
Ru1ka <wotb5ks@gmail.com>
2022-01-13 15:57:44 +0300

    Переделал структуру уровней, добавил уровень свободной игры.

--- Levels/FreeGame.py
+++ Levels/FreeGame.py
@@ -1,11 +1,36 @@
-import pygame
-from Entities.BobmModules.TimerModule.TimerModule import Timer
-from Entities.BobmModules.WiresModule.WiresModule import Wires
+from random import sample
+from Entities.BobmModules.BigButtonModule.BigButtonModule import BigButtonModule
+from Entities.BobmModules.KeyBoardModule.KeyboardModule import KeyboardModule
 from Entities.BobmModules.NoneModule.NoneModule import NoneModule
+from Entities.BobmModules.TimerModule.TimerModule import TimerModule
+from Entities.BobmModules.WiresModule.WiresModule import WiresModule
+from Entities.BobmModules.ButtonsModule.ButtonsModule import ButtonsModule
+from Entities.BobmModules.Ahmed_module.AhmedModule import AhmedModule
+from CONSTANTS import FPS, MODULES_COORDS
+from Levels.LevelClass import Level
 
 
-class Level1:
+class FreeLevel(Level):
     """Уровень 1. Содержит универсальную информацию о уровне."""
 
-    def __init__(self):
-        self.name = 'own'
+    types = [BigButtonModule, KeyboardModule, WiresModule, ButtonsModule, AhmedModule]
+
+    def __init__(self, time, modules_count, hardmode=False):
+        self.name = "FreeGame"
+        self.time = time * FPS
+        self.modules_count = modules_count  # от 1 до 5
+        self.mistakes = 2 if hardmode else 0
+
+    def generate_modules(self, bomb):
+        modules = [
+            TimerModule(bomb, MODULES_COORDS[0]).init(),
+            NoneModule(bomb, MODULES_COORDS[1]).init(),
+            NoneModule(bomb, MODULES_COORDS[2]).init(),
+            NoneModule(bomb, MODULES_COORDS[3]).init(),
+            NoneModule(bomb, MODULES_COORDS[4]).init(),
+            NoneModule(bomb, MODULES_COORDS[5]).init(),
+        ]
+        rand_modules = sample(self.types, self.modules_count)
+        # Поясню: Есть список КЛАССОВ! не объектов, мы перебираем его в цикле и получаются разные модули.
+        for m, module in zip(range(1, len(modules)), rand_modules):
+            modules[m] = module(bomb, module[m]._cords)
--- Levels/Level4.py
+++ Levels/Level4.py
@@ -1,5 +1,3 @@
-from random import choice, randint
-
 from CONSTANTS import MODULES_COORDS, FPS
 from Entities.BobmModules.BigButtonModule.BigButtonModule import BigButtonModule
 from Entities.BobmModules.KeyBoardModule.KeyboardModule import KeyboardModule
@@ -7,26 +5,14 @@ from Entities.BobmModules.NoneModule.NoneModule import NoneModule
 from Entities.BobmModules.TimerModule.TimerModule import TimerModule
 from Entities.BobmModules.WiresModule.WiresModule import WiresModule
 from Entities.BobmModules.ButtonsModule.ButtonsModule import ButtonsModule
-from Resources.BombGenerateInfo.BombSerialNum import (
-    SERIAL_NUMBERS_FIRST_SECTOR,
-    SERIAL_NUMBERS_THIRD_SECTOR,
-)
+from Levels.LevelClass import Level
 
 
-class Level4:
-    """Уровень 1. Содержит универсальную информацию о уровне."""
-
+class Level4(Level):
     def __init__(self):
         self.name = "4"
         self.mistakes = 0
         self.time = 300 * FPS
-
-    def generate_serial_number(self):
-        first = str(choice(SERIAL_NUMBERS_FIRST_SECTOR))
-        second = str(randint(1001, 9999))
-        third = str(choice(SERIAL_NUMBERS_THIRD_SECTOR))
-        fourth = str(randint(1001, 9999))
-        return [first, second, third, fourth]
 
     def generate_modules(self, bomb):
         # 6 модулей обязательно, обязательно в этом порядке.
@@ -39,9 +25,3 @@ class Level4:
             NoneModule(bomb, MODULES_COORDS[5]).init(),
         ]
         return modules
-
-    def generate_indicators(self):
-        return [randint(0, 1) == 0, randint(0, 1) == 0]
-
-    def generate_battery(self):
-        return randint(1, 3)
