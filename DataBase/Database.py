96086e05c37543ded7399c384884744dd9914e11
nastya milkhert <n.milkhert@gmail.com>
2022-01-14 18:46:20 +0300

    в бд добавила создание словаря с лучшем результатом по уровням

--- DataBase/Database.py
+++ DataBase/Database.py
@@ -5,13 +5,44 @@ class DataBase:
     def __init__(self, connection_string):
         self.con = sqlite3.connect(connection_string)
 
+    def best_time(self):
+        cur = self.con.cursor()
+        # словарь с уровнями и значениями лучшего времени
+        best_times_dict = {1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
+        # проходимся по всем значениям
+        for i in range(1, 7):
+            # берем результаты с одного уровня
+            values = cur.execute("""
+                SELECT * FROM all_information
+                WHERE level_id == ?
+                """, [i]).fetchall()
+            # усли результатов нет, то ничего не добавляем
+            if len(values) == 0:
+                pass
+            # если есть значения времени
+            else:
+                # проходимся по всем значениям и выбираем максимальное время
+                max_minuts = 0
+                max_sec = 0
+                for el in values:
+                    if el[2] > max_minuts:
+                        max_minuts = el[2]
+                        max_sec = el[3]
+                    elif el[2] == max_minuts and el[3] > max_sec:
+                        max_minuts = el[2]
+                        max_sec = el[3]
+                best_times_dict[i].append([max_minuts, max_sec])
+        return best_times_dict
+
     def insert_words(self, level_id, minuts, seconds):
         cur = self.con.cursor()
+        if level_id == 'own':
+            level_id = 6
         cur.execute("""INSERT INTO all_information(level_id, minuts, seconds) VALUES(?, ?, ?)
-        """, [level_id, minuts, seconds])
+                """, [level_id, minuts, seconds])
         self.con.commit()
 
-    def all_words(self):
+    def all_inf(self):
         cur = self.con.cursor()
         return cur.execute("""
                 SELECT * FROM all_information
